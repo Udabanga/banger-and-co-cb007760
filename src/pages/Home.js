c5ea29c4c545fcd7c3462c4dae3a59b021270296
udayanga <udayangajayamuthu@gmail.com>
2020-11-05 17:45:10 +0530

    Added carousel to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,77 @@
+import React, { Component } from 'react'
+import { Container } from 'react-bootstrap'
+// import { CarouselSlider } from './components/CarouselSlider'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-
-function Home() {
-  return (
-    <div>
-        <h1>Home</h1>
-    </div>
-  );
+export default class Home extends Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
+    return (
+      
+      <Container>
+        <h2> Responsive </h2>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+          <div>
+            <h3>7</h3>
+          </div>
+          <div>
+            <h3>8</h3>
+          </div>
+        </Slider>
+      </Container>
+    );
+  }
 }
-
-export default Home;
