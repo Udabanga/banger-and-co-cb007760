1418be3a13b00cbf20591713901b598e8f76b337
udayanga <udayangajayamuthu@gmail.com>
2020-11-05 17:46:07 +0530

    Fixed Navigation Car Listing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -13,7 +13,7 @@ function Navigation() {
                         <Nav.Link href="/">Home</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link href="/about">About</Nav.Link>
+                        <Nav.Link href="/CarListing">Car Listing</Nav.Link>
                     </Nav.Item>
                 </Nav> 
             </Navbar.Collapse>
